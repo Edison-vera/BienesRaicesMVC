51ef877df23645be1545f81ccc1c41334b6b84e7
Edison-vera <edisonefvc15@gmail.com>
2022-04-19 19:42:23 -0500

    egistrando URL's y los métodos asociados a ellas

--- public/index.php
+++ public/index.php
@@ -5,3 +5,10 @@ require_once __DIR__ . "../../includes/app.php";
 use MVC\Router;
 
 $router = new Router();
+
+$router->get("/nosotros", "funcion_nosotros");
+$router->get("/nosassaotros", "funcion_nosotros");
+$router->get("/", "funcion_nosotros");
+
+
+$router->comprobarRutas();
