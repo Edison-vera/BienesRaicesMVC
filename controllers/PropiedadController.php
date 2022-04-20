988086b9b5bd0f24ece2641237cf407d0190d7b7
Edison-vera <edisonefvc15@gmail.com>
2022-04-19 19:56:29 -0500

    Asociando URL's a métodos del Controlador

---
+++ controllers/PropiedadController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Controllers;
+
+class propiedadController{
+
+    public static function index(){
+        echo "Index";
+
+    }
+    public static function crear(){
+        echo "Crear";
+
+    }
+    public static function actualizar(){
+        echo "Actualizar";
+
+    }
+}
