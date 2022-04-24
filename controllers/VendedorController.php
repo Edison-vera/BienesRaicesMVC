ba433ce09314f571e94370fecb17755299299b55
Edison-vera <edisonefvc15@gmail.com>
2022-04-24 18:35:19 -0500

    Mostrando el Formulario para crear vendedores

--- controllers/VendedorController.php
+++ controllers/VendedorController.php
@@ -5,8 +5,15 @@ use MVC\Router;
 use Model\Vendedor;
 
 class VendedorController {
-    public static function crear(){
-          echo "Crear vendedor";
+    public static function crear(Router $router){
+
+          $errores = Vendedor::getErrores();
+          $vendedor = new Vendedor;
+          $router->render("vendedores/crear", [
+              "errores"=> $errores,
+              "vendedor"=> $vendedor
+
+          ]);
     }
     public static function actualizar(){
          echo "Actualizar vendedor";
