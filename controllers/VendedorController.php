4c3ae3f1d0b342f8d79033f26d9ab3ead5e5c5ac
Edison-vera <edisonefvc15@gmail.com>
2022-04-24 18:20:18 -0500

    Creando el Controlador para Vendedores

---
+++ controllers/VendedorController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Controllers;
+use MVC\Router;
+use Model\Vendedor;
+
+class VendedorController {
+    public static function crear(){
+          echo "Crear vendedor";
+    }
+    public static function actualizar(){
+         echo "Actualizar vendedor";
+    }
+    public static function eliminar(){
+        echo "Eliminar vendedor";
+    }
+}
+
+
