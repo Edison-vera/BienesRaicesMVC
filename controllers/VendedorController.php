1ca41e62fd2c7808f40fe396171b6078007a8a3a
Edison-vera <edisonefvc15@gmail.com>
2022-04-25 20:31:03 -0500

    Almacenando Vendedores en la Base de datos

--- controllers/VendedorController.php
+++ controllers/VendedorController.php
@@ -32,8 +32,39 @@ class VendedorController {
 
           ]);
     }
-    public static function actualizar(){
-         echo "Actualizar vendedor";
+    public static function actualizar(Router $router){
+        
+        $errores = Vendedor::getErrores();
+        $id= validarORedireccionar("/index.php/admin");
+        
+        //Obtener datos del vendedor para actualizar
+
+        $vendedor = Vendedor::find($id);
+        //Ejecutar el codigo despues que el usuario envia el formulario 
+     if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    
+       //Asignar los valores 
+       $args = $_POST['vendedor'];
+
+
+       //Sicronizar objeto en memoria
+       $vendedor->sincronizar($args);
+   
+       //Validacion
+       $errores = $vendedor->validar();
+    
+   
+     if(empty($errores)){
+        $vendedor->guardar();
+    }
+
+}
+
+          $router->render("vendedores/actualizar",[
+              "errores"=>$errores,
+              "vendedor"=>$vendedor
+
+          ]);
     }
     public static function eliminar(){
         echo "Eliminar vendedor";
--- views/propiedades/admin.php
+++ views/propiedades/admin.php
@@ -77,7 +77,7 @@
 
                         <input type="submit"  class="boton-rojo-block" value="Eliminar">
                    </form>
-                    <a href="../../BienesRaices/admin/vendedores/actualizar.php?id=<?php echo $vendedor->id; ?>" class="boton-amarillo-block">Actualizar</a>    
+                    <a href="../index.php/vendedores/actualizar?id=<?php echo $vendedor->id; ?>" class="boton-amarillo-block">Actualizar</a>    
                 </td>
             </tr>
             <?php endforeach; ?>
