5a56973f26d4b9159fed00d75ac305725916228e
Edison-vera <edisonefvc15@gmail.com>
2022-04-29 20:56:33 -0500

    Configurando Mailtrap y enviando un email

--- controllers/PaginasController.php
+++ controllers/PaginasController.php
@@ -3,6 +3,8 @@
 namespace Controllers;
 use MVC\Router;
 use Model\Propiedad;
+use PHPMailer\PHPMailer\PHPMailer;
+
 class PaginasController{
     
     public static function index(Router $router){
@@ -53,7 +55,41 @@ class PaginasController{
     public static function contacto(Router $router){
         
         if($_SERVER["REQUEST_METHOD"] ==="POST"){
-            debugear($_POST);
+            
+            //Crear una instancia de PHPMailer
+            $mail = new PHPMailer();
+
+            //Configurar SMTP
+            $mail->isSMTP();
+            $mail->Host= "smtp.mailtrap.io";
+            $mail->SMTPAuth = true;
+            $mail->Username = "3c6036537c7e83";
+            $mail->Password = "004c8988010838";
+            $mail->SMTPSecure = "tls";
+            $mail->Port = 2525;
+
+            //Configurar el contenido del mail
+            $mail->setFrom("admin@bienesraices.com");
+            $mail->addAddress("admin@bienesraices.com", "BienesRaices.com");
+            $mail->Subject =("Tienes un nuevo mensaje");
+
+            //Habilitar HTML
+            $mail->isHTML(true);
+            $mail->CharSet = "UTF-8";
+
+            //Definir el contenido
+            $contenido = "<html> <p> Tienes un nuevo mensaje </p></html>";
+
+            $mail->Body = $contenido;
+            $mail->AltBody= "Esto es texto alternativo sin HTML";
+
+            //Enviar el email
+            if($mail->send()){
+                echo "Mensaje enviado correctamente";
+            }else{
+                echo "El mensaje no se pudo enviar";
+            }
+
         }
         $router->render("paginas/contacto",[
 
