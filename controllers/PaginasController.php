e970b3d04b23dedf65f4598ec79e93e0109b4069
Edison-vera <edisonefvc15@gmail.com>
2022-04-29 20:06:41 -0500

    Creando la página de Blog

--- controllers/PaginasController.php
+++ controllers/PaginasController.php
@@ -38,13 +38,23 @@ class PaginasController{
         "propiedad" => $propiedad
         ]);
     }
-    public static function blog(){
-        echo "Desde blog";
+    public static function blog(Router $router){
+        
+        $router->render("paginas/blog",[
+          
+        ]);
     }
-    public static function entrada(){
-        echo "Desde entrada";
+    public static function entrada(Router $router){
+        
+        $router->render("paginas/entrada",[
+
+        ]);
     }
-    public static function contacto(){
-        echo "Desde contacto";
+    public static function contacto(Router $router){
+
+        $router->render("paginar/contacto",[
+
+        ]);
+        
     }
 }
