c9e86b39454db327dad0321d3173ae3f59f28a6c
Edison-vera <edisonefvc15@gmail.com>
2022-05-30 21:13:05 -0500

    Comprobando que un elemento exista

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -3,6 +3,8 @@
 describe('Carga la pagina principal', () => {
     it('Prueba el header de la pagina principal', () => {
         cy.visit('/');
-        cy.get('[data-cy="heading-sitio"]')
+        cy.get('[data-cy="heading-sitio"]').should('exist');
+        cy.get('[data-cy="heading-sitio"]').invoke('text').should('equal', ' Venta de casas y departamientos exclusivos de lujo ');
+        cy.get('[data-cy="heading-sitio"]').invoke('text').should('not.equal', 'Bienes Raices');
     });
 });
