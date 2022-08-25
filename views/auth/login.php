c32f8250b3a0ef2974da98169e074d49f4223ff8
Edison-vera <edisonefvc15@gmail.com>
2022-08-24 20:29:03 -0500

    Validando de la creación de usuarios

--- views/auth/login.php
+++ views/auth/login.php
@@ -4,7 +4,6 @@
     <?php foreach ($errores as $error): ?>
     <div data-cy="alerta-login" class="alerta error"><?php echo $error; ?></div>
 
-
     <?php endforeach; ?>
 
     <form data-cy="formulario-login" method="POST" class="formulario" novalidate >
